ff73254ce9e8ef8a5620a020fcfd77a9a45d07bf
Wilfer Salas <sony634@hotmail.com>
2022-05-26 14:47:46 -0500

    add form validation

--- src/components/entry-card/CardForm.tsx
+++ src/components/entry-card/CardForm.tsx
@@ -6,16 +6,18 @@ import FormControl from '@mui/material/FormControl';
 import IconButton from '@mui/material/IconButton';
 import MenuItem from '@mui/material/MenuItem';
 import Paper from '@mui/material/Paper';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Tooltip from '@mui/material/Tooltip';
-import { FC, useState, ChangeEvent } from 'react';
+import { FC } from 'react';
 import { red, green } from '@mui/material/colors';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 
 // @scripts
 import { Entry } from '../../interfaces';
 import { priorityIcons } from './CardItem';
+import { Typography } from '@mui/material';
 
 // @interfaces
 interface Props {
@@ -25,21 +27,27 @@ interface Props {
   type: Entry['status'];
 }
 
+interface FormInput {
+  title: string;
+  priority: {label: string; value: string };
+}
+
 const CardForm: FC<Props> = ({
   onClose,
   onSave,
   openFormIssue,
   type,
 }) => {
-  const [value, setValue] = useState('');
-  const [select, setSelect] = useState('medium');
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm();
 
-  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
-
-  const handleOnSelect = (event: SelectChangeEvent) => {
-    setSelect(event.target.value as string);
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
+    console.log(data);
+    onSave();
   };
 
   return (
@@ -54,12 +62,21 @@ const CardForm: FC<Props> = ({
         }}
         variant="outlined"
       >
-        <TextField
-          fullWidth
-          onChange={handleOnChange}
-          placeholder="Add a title..."
-          size="small"
-          value={value}
+        <Controller
+          control={control}
+          defaultValue=""
+          name="title"
+          rules={{ required: true }}
+          render={(({ field }) => (
+            <TextField
+              error={Boolean(errors.title)}
+              fullWidth
+              placeholder="Add a title..."
+              size="small"
+              {...register('title', { required: true, minLength: 1 })}
+              {...field}
+            />
+          ))}
         />
         <Stack
           alignItems="center"
@@ -67,15 +84,22 @@ const CardForm: FC<Props> = ({
           justifyContent="space-between"
           sx={{ mt: 2 }}
         >
-          <FormControl size="small">
-            <Select onChange={handleOnSelect} value={select}>
-              {Object.keys(priorityIcons).map((icon) => (
-                <MenuItem key={icon} value={icon}>
-                  {priorityIcons[icon]}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+          <Controller
+            control={control}
+            defaultValue="medium"
+            name="priority"
+            render={(({ field }) => (
+              <FormControl size="small">
+                <Select {...field}>
+                  {Object.keys(priorityIcons).map((icon) => (
+                    <MenuItem key={icon} value={icon}>
+                      {priorityIcons[icon]}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            ))}
+          />
           <Box>
             <Tooltip title="Cancel">
               <IconButton onClick={onClose}>
@@ -83,7 +107,7 @@ const CardForm: FC<Props> = ({
               </IconButton>
             </Tooltip>
             <Tooltip title="Save">
-              <IconButton onClick={onSave}>
+              <IconButton onClick={handleSubmit(onSubmit)}>
                 <CheckIcon sx={{ color: green[700] }} />
               </IconButton>
             </Tooltip>
