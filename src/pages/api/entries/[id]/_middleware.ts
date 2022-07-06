9e524a1841ec712f59fcdd5439cc91d042914936
Wilfer Salas <sony634@hotmail.com>
2022-07-05 20:23:36 -0500

    change middleware

--- src/pages/api/entries/[id]/_middleware.ts
+++ src/pages/api/entries/[id]/_middleware.ts
@@ -2,7 +2,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 const middleware = (req: NextRequest) => {
-  const id = req.page?.params?.id!;
+  const id = req?.page?.params?.id;
   const checkMongoIDRegExp = /^[0-9a-fA-F]{24}$/;
 
   if (id && !checkMongoIDRegExp.test(id as string)) {
