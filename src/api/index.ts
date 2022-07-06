29ee81eeca0c64ec4369c853c9cd15ce8bd709a2
Wilfer Salas <sony634@hotmail.com>
2022-07-05 20:36:24 -0500

    update api logic

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,26 +6,28 @@ import { useQuery } from 'react-query';
 // @interfaces
 import { Entry } from '../interfaces';
 
-const client = '/api';
+const entriesApi = axios.create({
+  baseURL: '/api',
+});
 
-export const addEntry = (entry: Entry) => axios.post<Entry>(`${client}/entries`, { entry }).then((res) => res.data);
+export const addEntry = (entry: Entry) => entriesApi.post<Entry>('/entries', { entry }).then((res) => res.data);
 
 export const updateEntry = (id: Entry['_id'], entry: Entry) =>
-  axios.put<Entry>(`${client}/entries/${id}`, { entry }).then((res) => res.data);
+  entriesApi.put<Entry>(`/entries/${id}`, { entry }).then((res) => res.data);
 
 export const updateStatus = (id: Entry['_id'], status: Entry['status']) =>
-  axios.put<Entry>(`${client}/entries/${id}`, { status }).then((res) => res.data);
+  entriesApi.put<Entry>(`/entries/${id}`, { status }).then((res) => res.data);
 
 export const deleteEntry = (id: Entry['_id']) =>
-  axios.delete<Entry>(`${client}/entries/${id}`).then((res) => res.data);
+  entriesApi.delete<Entry>(`/entries/${id}`).then((res) => res.data);
 
 export const getEntry = (id: string) =>
-  axios.get<Entry>(`${client}/entries/${id}`).then((res) => res.data);
+  entriesApi.get<Entry>(`/entries/${id}`).then((res) => res.data);
 
 export const useFetchEntries = () =>
   useQuery<Entry[], Error>('getEntries', () =>
-    axios.get<Entry[]>(`${client}/entries`).then((res) => res.data));
+    entriesApi.get<Entry[]>('entries').then((res) => res.data));
 
 export const useFetchEntry = (id: string) =>
   useQuery<Entry, Error>('getEntry', () =>
-    axios.get<Entry>(`${client}/entries/${id}`).then((res) => res.data));
+    entriesApi.get<Entry>(`/entries/${id}`).then((res) => res.data));
